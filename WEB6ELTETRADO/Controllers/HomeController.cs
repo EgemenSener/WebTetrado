9dba4dd7fc8c3e035162e5c156c122c065e9a36a
Egemen <egemensener98@gmail.com>
2021-10-03 15:14:48 +0300

    checkpoint

--- WEB6ELTETRADO/Controllers/HomeController.cs
+++ WEB6ELTETRADO/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB6ELTETRADO.Models;
 
 namespace WEB6ELTETRADO.Controllers
 {
@@ -10,9 +11,14 @@ namespace WEB6ELTETRADO.Controllers
     {
         public ActionResult Index()
         {
+            List<Urun> urunler = new List<Urun>()
+            {
+                new Urun(){UrunId=1,UrunAdi="Samsung"},
+                new Urun(){UrunId=2,UrunAdi="IPhone"}
+
+            };
             
-            
-            return View();
+            return View(urunler);
         }
 
        
---
+++ WEB6ELTETRADO/Models/Urun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB6ELTETRADO.Models
+{
+    public class Urun
+    {
+        public int UrunId { get; set; }
+        public string UrunAdi { get; set; }
+
+    }
+}
