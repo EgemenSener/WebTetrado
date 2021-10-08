fd9eeb40424e95b8ac50c9e2d531dc58acdc5fd1
Egemen <egemensener98@gmail.com>
2021-10-08 21:27:50 +0300

    $ project changed to javascript and first function added!

---
+++ deneme.js
@@ -0,0 +1,27 @@
+﻿function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+function checkFunction() {
+
+    var checkBox = document.getElementById("myCheck1");
+    if (checkBox.checked == true) {
+        
+        console.log("true");
+                  
+    }
+    else {
+        
+        console.log("false");
+    }
+
+}
