36d3afa3448426e87d6ed0b139a0b08662325d64
Egemen <egemensener98@gmail.com>
2021-10-09 16:51:34 +0300

    checkbox functions return correct parameters

--- deneme.js
+++ deneme.js
@@ -11,17 +11,49 @@
         text.style.display = "none";
     }
 }
-function checkFunction() {
+var checkBox1 = false;
+var checkBox2 = false;
+var checkBox3 = false;
 
-    var checkBox = document.getElementById("myCheck1");
-    if (checkBox.checked == true) {
-        
-        console.log("true");
-                  
+function checkFunction1() {
+
+    if(document.getElementById("myCheck1").checked){
+
+         checkBox1 = true;
+         return checkBox1;
     }
-    else {
-        
-        console.log("false");
+    else{
+        checkBox1 = false;
+         return checkBox1;
     }
+}
+function checkFunction2(){
+    
+    if(document.getElementById("myCheck2").checked){
+        
+        checkBox2 = true;
+         return checkBox2;
+    }
+    else{
+        checkBox2 = false;
+         return checkBox2;
+    }
+}
+function checkFunction3() {
+
+    if(document.getElementById("myCheck3").checked){
+        
+        checkBox3 = true;
+         return checkBox3;
+    }
+    else{
+        checkBox3 = false;
+         return checkBox3;
+    }
+}
+function printFunction(){
+
+    console.log("Checkbox1 is "+ checkBox1 +" Checkbox2 is " + checkBox2 + " Checkbox3 is "+checkBox3);
+
 
 }
