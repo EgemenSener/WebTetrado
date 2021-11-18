06ed362ce87609f9bcd49c99db688e02fa9593b0
Egemen <egemensener98@gmail.com>
2021-11-18 17:52:16 +0100

    taking variables different method added!

--- script.js
+++ script.js
@@ -86,96 +86,100 @@ function getResponse() {
     fetch("http://localhost:8000/result/:id")
         .then(response => response.json())
         .then(data => {
-            // console.log(data);
-            const basePairs = data.basePairs[0];
-            const helicesQuadruplexes = data.helices[0].quadruplexes[0];
-            const hQLoops = helicesQuadruplexes.loops[0];
-            const hQtetrads = helicesQuadruplexes.tetrads[0];
-            const hQtracts = helicesQuadruplexes.tracts[0];
-            const helicesTetradPairs = data.helices[0].tetradPairs[0];
-            const metals = data.metals[0];
-            const nucleotides = data.nucleotides[0];
-            // const textBasePairs  = JSON.stringify(basePairs);
-            console.log(data);
-            // console.log(metals)
-            document.getElementById("getResultsPanel").innerHTML += `<div
-            style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
-             <h4><u>BASEPAIRS</u></h4>
-             <p>edge3="${basePairs.edge3}"</p>
-             <p>edge5="${basePairs.edge5}"</p>
-             <p>nt1="${basePairs.nt1}"</p>
-             <p>nt2="${basePairs.nt2}"</p>
-             <p>stericity="${basePairs.stericity}"</p></div>` + `<p></p>` +
-                `<div
-            style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
-                <h4><u>HELICES</u></h4>
-                <p>QUADRUPLEXES:
-                    <p style="margin-left: 10px;">gbaClassification="${helicesQuadruplexes.gbaClassification}"</p>
-                    <p style="margin-left: 10px;">loopClassification="${helicesQuadruplexes.loopClassification}"</p>
-                    <p style="margin-left: 10px;">LOOPS:
+            document.getElementById("json").textContent = JSON.stringify(data, undefined, 2);
+            
+        })
+        // .then(data => {
+        //     // console.log(data);
+        //     const basePairs = data.basePairs[0];
+        //     const helicesQuadruplexes = data.helices[0].quadruplexes[0];
+        //     const hQLoops = helicesQuadruplexes.loops[0];
+        //     const hQtetrads = helicesQuadruplexes.tetrads[0];
+        //     const hQtracts = helicesQuadruplexes.tracts[0];
+        //     const helicesTetradPairs = data.helices[0].tetradPairs[0];
+        //     const metals = data.metals[0];
+        //     const nucleotides = data.nucleotides[0];
+        //     // const textBasePairs  = JSON.stringify(basePairs);
+        //     console.log(data);
+        //     // console.log(metals)
+        //     document.getElementById("getResultsPanel").innerHTML += `<div
+        //     style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
+        //      <h4><u>BASEPAIRS</u></h4>
+        //      <p>edge3="${basePairs.edge3}"</p>
+        //      <p>edge5="${basePairs.edge5}"</p>
+        //      <p>nt1="${basePairs.nt1}"</p>
+        //      <p>nt2="${basePairs.nt2}"</p>
+        //      <p>stericity="${basePairs.stericity}"</p></div>` + `<p></p>` +
+        //         `<div
+        //     style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
+        //         <h4><u>HELICES</u></h4>
+        //         <p>QUADRUPLEXES:
+        //             <p style="margin-left: 10px;">gbaClassification="${helicesQuadruplexes.gbaClassification}"</p>
+        //             <p style="margin-left: 10px;">loopClassification="${helicesQuadruplexes.loopClassification}"</p>
+        //             <p style="margin-left: 10px;">LOOPS:
                         
-                        <p style="margin-left: 20px;">type="${hQLoops.type}"</p>
-                        <p style="margin-left: 20px;">NUCLEOTIDES:
-                            <p style="margin-left: 30px;">0="${hQLoops.nucleotides[0]}"</p>
+        //                 <p style="margin-left: 20px;">type="${hQLoops.type}"</p>
+        //                 <p style="margin-left: 20px;">NUCLEOTIDES:
+        //                     <p style="margin-left: 30px;">0="${hQLoops.nucleotides[0]}"</p>
 
-                        </p>
-                    </p>
-                    <p style="margin-left: 10px;">onzm="${helicesQuadruplexes.onzm}"</p>
-                    <p style="margin-left: 10px;">TETRADS:
-                        <p style="margin-left: 20px;">gbaClassification="${hQtetrads.gbaClassification}"</p>
-                        <p style="margin-left: 20px;">id="${hQtetrads.id}"</p>
-                        <p style="margin-left: 20px;">IONSCHANNEL:
-                            <p style="margin-left: 30px;">0="${hQtetrads.ionsChannel[0]}"</p>
+        //                 </p>
+        //             </p>
+        //             <p style="margin-left: 10px;">onzm="${helicesQuadruplexes.onzm}"</p>
+        //             <p style="margin-left: 10px;">TETRADS:
+        //                 <p style="margin-left: 20px;">gbaClassification="${hQtetrads.gbaClassification}"</p>
+        //                 <p style="margin-left: 20px;">id="${hQtetrads.id}"</p>
+        //                 <p style="margin-left: 20px;">IONSCHANNEL:
+        //                     <p style="margin-left: 30px;">0="${hQtetrads.ionsChannel[0]}"</p>
 
-                        </p>
-                        <p style="margin-left: 20px;">IONSOUTSIDE:
-                            <p style="margin-left: 30px;">ion="${hQtetrads.ionsOutside[0].ion}"</p>
-                            <p style="margin-left: 30px;">nt="${hQtetrads.ionsOutside[0].nt}"</p>
+        //                 </p>
+        //                 <p style="margin-left: 20px;">IONSOUTSIDE:
+        //                     <p style="margin-left: 30px;">ion="${hQtetrads.ionsOutside[0].ion}"</p>
+        //                     <p style="margin-left: 30px;">nt="${hQtetrads.ionsOutside[0].nt}"</p>
 
-                        </p>
-                        <p style="margin-left: 20px;">nt1="${hQtetrads.nt1}"</p>
-                        <p style="margin-left: 20px;">nt2="${hQtetrads.nt2}"</p>
-                        <p style="margin-left: 20px;">nt3="${hQtetrads.nt3}"</p>
-                        <p style="margin-left: 20px;">nt4="${hQtetrads.nt4}"</p>
-                        <p style="margin-left: 20px;">onz="${hQtetrads.onz}"</p>
-                        <p style="margin-left: 20px;">planarityDeviation="${hQtetrads.planarityDeviation}"</p>
+        //                 </p>
+        //                 <p style="margin-left: 20px;">nt1="${hQtetrads.nt1}"</p>
+        //                 <p style="margin-left: 20px;">nt2="${hQtetrads.nt2}"</p>
+        //                 <p style="margin-left: 20px;">nt3="${hQtetrads.nt3}"</p>
+        //                 <p style="margin-left: 20px;">nt4="${hQtetrads.nt4}"</p>
+        //                 <p style="margin-left: 20px;">onz="${hQtetrads.onz}"</p>
+        //                 <p style="margin-left: 20px;">planarityDeviation="${hQtetrads.planarityDeviation}"</p>
                         
                     
-                    </p>
-                    <p style="margin-left: 10px;">TRACTS:
-                        <p style="margin-left: 20px;">0="${hQtracts[0]}"</p>
+        //             </p>
+        //             <p style="margin-left: 10px;">TRACTS:
+        //                 <p style="margin-left: 20px;">0="${hQtracts[0]}"</p>
            
-                    </p>
-                </p>
-                <p>TETRADPAIRS:
-                    <p style="margin-left: 10px;">direction="${helicesTetradPairs.direction}"</p>
-                    <p style="margin-left: 10px;">rise="${helicesTetradPairs.rise}"</p>
-                    <p style="margin-left: 10px;">tetrad1="${helicesTetradPairs.tetrad1}"</p>
-                    <p style="margin-left: 10px;">tetrad2="${helicesTetradPairs.tetrad2}"</p>
-                    <p style="margin-left: 10px;">twist="${helicesTetradPairs.twist}"</p>
+        //             </p>
+        //         </p>
+        //         <p>TETRADPAIRS:
+        //             <p style="margin-left: 10px;">direction="${helicesTetradPairs.direction}"</p>
+        //             <p style="margin-left: 10px;">rise="${helicesTetradPairs.rise}"</p>
+        //             <p style="margin-left: 10px;">tetrad1="${helicesTetradPairs.tetrad1}"</p>
+        //             <p style="margin-left: 10px;">tetrad2="${helicesTetradPairs.tetrad2}"</p>
+        //             <p style="margin-left: 10px;">twist="${helicesTetradPairs.twist}"</p>
 
-                </p>
-            </div>` + `<p></p>` +
-                `<div
-            style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
-            <h4><u>METALS</u></h4>
-                <p>count="${metals.count}"</p>
-                <p>symbol="${metals.symbol}"</p>
-            </div>` + `<p></p>` +
-                `<div
-            style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
-             <h4><u>NUCLEOTIDES</u></h4>
-             <p>chain="${nucleotides.chain}"</p>
-             <p>chi="${nucleotides.chi}"</p>
-             <p>fullName="${nucleotides.fullName}"</p>
-             <p>glycosidicBond="${nucleotides.glycosidicBond}"</p>
-             <p>icode="${nucleotides.icode}"</p>
-             <p>index="${nucleotides.index}"</p>
-             <p>model="${nucleotides.model}"</p>
-             <p>molecule="${nucleotides.molecule}"</p>
-             <p>number="${nucleotides.number}"</p>
-             <p>shortName="${nucleotides.shortName}"</p></div>` + `<p></p>`
-        })
+        //         </p>
+        //     </div>` + `<p></p>` +
+        //         `<div
+        //     style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
+        //     <h4><u>METALS</u></h4>
+        //         <p>count="${metals.count}"</p>
+        //         <p>symbol="${metals.symbol}"</p>
+        //     </div>` + `<p></p>` +
+        //         `<div
+        //     style="position: relative; border: 2px solid #062bbe;font-weight: bold;min-width: 40%;height:100%; padding:3px;line-height: 200%;">
+        //      <h4><u>NUCLEOTIDES</u></h4>
+        //      <p>chain="${nucleotides.chain}"</p>
+        //      <p>chi="${nucleotides.chi}"</p>
+        //      <p>fullName="${nucleotides.fullName}"</p>
+        //      <p>glycosidicBond="${nucleotides.glycosidicBond}"</p>
+        //      <p>icode="${nucleotides.icode}"</p>
+        //      <p>index="${nucleotides.index}"</p>
+        //      <p>model="${nucleotides.model}"</p>
+        //      <p>molecule="${nucleotides.molecule}"</p>
+        //      <p>number="${nucleotides.number}"</p>
+        //      <p>shortName="${nucleotides.shortName}"</p></div>` + `<p></p>`
+        // })
 }
 let companies = [
 
